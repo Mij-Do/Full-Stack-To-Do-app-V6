998283028dec836576cd243ae80d6e3279159586
ahmed <ahmedmijdo2@gmail.com>
2026-07-19 14:00:48 +0300

    dynamic table todos

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,12 @@ import { getTodoListAction } from "@/prisma/actions/todo.action";
 
 export default async function Home() {
   const todos = await getTodoListAction();
-  
-
 
   return (
     // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <div className="container">  
-      <TodoTable />
-      <ul>
-        {todos.map(todo => <li key={todo.id}> {todo.title} </li>)}
-      </ul>
       <AddTodoForm />
+      <TodoTable todos={todos}/>
     </div>
   );
 }
--- components/todoTable.tsx
+++ components/todoTable.tsx
@@ -10,62 +10,20 @@ import {
 } from "@/components/ui/table"
 import { Button } from "./ui/button"
 import { Pen, Trash } from "lucide-react"
-import { getTodoListAction } from "@/prisma/actions/todo.action";
+import { ITodos } from "@/interfaces";
+import { Badge } from "./ui/badge";
 
-const invoices = [
-    {
-        invoice: "INV001",
-        paymentStatus: "Paid",
-        totalAmount: "$250.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV002",
-        paymentStatus: "Pending",
-        totalAmount: "$150.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV003",
-        paymentStatus: "Unpaid",
-        totalAmount: "$350.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV004",
-        paymentStatus: "Paid",
-        totalAmount: "$450.00",
-        paymentMethod: "Credit Card",
-    },
-    {
-        invoice: "INV005",
-        paymentStatus: "Paid",
-        totalAmount: "$550.00",
-        paymentMethod: "PayPal",
-    },
-    {
-        invoice: "INV006",
-        paymentStatus: "Pending",
-        totalAmount: "$200.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV007",
-        paymentStatus: "Unpaid",
-        totalAmount: "$300.00",
-        paymentMethod: "Credit Card",
-    },
-]
+interface IProps {
+    todos: ITodos[];
+}
 
-const todos = await getTodoListAction();
-
-export default function TodoTable() {
+export default function TodoTable({todos}: IProps) {
     return (
         <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
             <TableRow>
-                <TableHead className="w-[100px]">ID</TableHead>
+                <TableHead className="w-25">ID</TableHead>
                 <TableHead>Title</TableHead>
                 <TableHead>Body</TableHead>
                 <TableHead>Completed</TableHead>
@@ -78,7 +36,9 @@ export default function TodoTable() {
                 <TableCell className="font-medium">{todo.id}</TableCell>
                 <TableCell>{todo.title}</TableCell>
                 <TableCell>{todo.body}</TableCell>
-                <TableCell>{todo.completed}</TableCell>
+                <TableCell>
+                    {todo.completed ? <Badge>Completed</Badge> : <Badge variant={"destructive"}>UnCompleted</Badge> }
+                </TableCell>
                 <TableCell className="flex items-center justify-end space-x-2">
                     <Button size={"icon"}>
                         <Pen size={16}/>
